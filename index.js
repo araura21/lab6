186b642b4a6e1430662c8ee911efbaac8c146e78
Andrea Katherine Raura Negrete <rauraandrea0@gmail.com>
2026-01-27 11:53:37 -0500

    Proyecto base para realizar CI

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+app.get('/', (_req, res)=>{
+    res.send("Integracion continua funcionando!")
+});
+
+app.listen(PORT, ()=> {
+    console.log(`Servidor ejecutandose en el puerto ${PORT}`);
+});
