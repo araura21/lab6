c65ca05386c59dd747b9bdb9c255bfab2b6c0d54
Andrea Katherine Raura Negrete <rauraandrea0@gmail.com>
2026-02-08 14:38:39 -0500

    provocar error en math.js

--- math.js
+++ math.js
@@ -1,6 +1,6 @@
 const factorial = (n) => {
-    if (n < 0) return null;
-    //if (n === 0 || n === 1) return 1;
+    //if (n < 0) return null;
+    if (n === 0 || n === 1) return 1;
     let result = 1;
     for (let i = 2; i <= n; i++) {
         result *= i;
@@ -9,8 +9,8 @@ const factorial = (n) => {
 };
 
 const fibonacci = (n) => {
-    if (n < 0) return null;
-    //if (n === 0) return 0; 
+    //if (n < 0) return null;
+    if (n === 0) return 0; 
     if (n === 1) return 1;
     let a = 0, b = 1;
     for (let i = 2; i <= n; i++) {
