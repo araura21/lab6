9ffe184e3ab20db7cb8818cb5c7bc5b27f5df6e3
Andrea Katherine Raura Negrete <rauraandrea0@gmail.com>
2026-02-08 14:30:54 -0500

    provocar error en math.js

--- math.test.js
+++ math.test.js
@@ -7,9 +7,9 @@ describe('Pruebas unitarias para funciones matemáticas', () => {
         expect(factorial(5)).toBe(120);
     });
 
-    /*test('El factorial de 0 debe ser 1', () => {
+    test('El factorial de 0 debe ser 1', () => {
         expect(factorial(0)).toBe(1);
-    });*/
+    });
 
     test('Debe retornar null al calcular negativos', () => {
         expect(factorial(-1)).toBeNull();
@@ -20,9 +20,9 @@ describe('Pruebas unitarias para funciones matemáticas', () => {
         expect(fibonacci(5)).toBe(5);
     });
 
-    /*test('El primer número de Fibonacci debe ser 1', () => {
+    test('El primer número de Fibonacci debe ser 1', () => {
         expect(fibonacci(1)).toBe(1);
-    });*/
+    });
 
     test('Debe retornar null para números negativos en fibonacci', () => {
         expect(fibonacci(-5)).toBeNull();
